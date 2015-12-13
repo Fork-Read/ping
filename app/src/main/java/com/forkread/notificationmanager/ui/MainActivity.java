516de546f73e60460c85ab1d1cb2108a05c9f4ce
Prateek Agarwal <prateekagr98@gmail.com>
2015-12-13 11:50:08 +0530

    Create tab layout

--- app/src/main/java/com/forkread/notificationmanager/ui/MainActivity.java
+++ app/src/main/java/com/forkread/notificationmanager/ui/MainActivity.java
@@ -16,11 +16,15 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
 import com.forkread.notificationmanager.R;
 import com.forkread.notificationmanager.adapters.NotificationCursorAdapter;
 import com.forkread.notificationmanager.database.DatabaseContracts;
 import com.forkread.notificationmanager.service.ForkReadNotificationListenerService;
+
 
 /**
  * Created by dipesh on 03/08/15. Edited by Prateek 01/12/15
@@ -32,21 +36,53 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.notifications_list);
+//        mRecyclerView = (RecyclerView) findViewById(R.id.notifications_list);
+//        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        mAdapter = new NotificationCursorAdapter(this, null);
+//        mRecyclerView.setAdapter(mAdapter);
+//        Intent service = new Intent(this, ForkReadNotificationListenerService.class);
+//        startService(service);
+//        startLoading();
+//        registerContentObserver();
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.notifications_list);
-        mRecyclerView = (RecyclerView) findViewById(R.id.notifications_list);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mAdapter = new NotificationCursorAdapter(this, null);
-        mRecyclerView.setAdapter(mAdapter);
-        Intent service = new Intent(this, ForkReadNotificationListenerService.class);
-        startService(service);
-        startLoading();
-        registerContentObserver();
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Notification"));
+        tabLayout.addTab(tabLayout.newTab().setText("Settings"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_activity, menu);
+//        getMenuInflater().inflate(R.menu.main_activity, menu);
         return true;
     }
 
@@ -57,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
 //            startActivity(activity);
 //            return true;
 //        }
-        return false;
+        return super.onOptionsItemSelected(item);
     }
 
     private LoaderManager.LoaderCallbacks<Cursor> LOADER_CALLBACKS = new LoaderManager.LoaderCallbacks<Cursor>() {
